8befaee962151ce3961f84de8d34fc0a3691be29
jungsehwan <jungsehwan0612@gmail.com>
2019-12-06 01:00:50 +0900

    191205| props까지 학습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+function Food(props) {
+  return (
+  <h1>I love { props.fav }</h1>
+  );
+}
+
 function App() {
-  return <div>
-    <h1>Hello</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi"/>
+      <Food fav="라면"/>
+      <Food fav="삼겹살"/>
+      <Food fav="쭈꾸미"/>
+    </div>
+  );
 }
 
 export default App;
