f41c840a7d6297227ef90c7c3c4169470201a11e
jungsehwan <jungsehwan0612@gmail.com>
2019-12-06 16:56:53 +0900

    191206| props 끝, PropTypes까지 학습 완료

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food(props) {
+
+const foodILike = [
+  {
+    key: 1,
+    name: "김치",
+    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5.0
+  },
+  
+  {
+    key: 2,
+    name: "삼겹살",
+    image: "https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg",
+    rating: 4.0
+  },
+  
+  {
+    key: 3,
+    name: "라면",
+    image: "http://file.mk.co.kr/meet/neds/2017/09/image_readtop_2017_587233_15042337473013492.jpg",
+    rating: "3.7"
+  }
+]
+
+// function renderFood(dish) {
+  //   // console.log(dish)
+  //   return <Food name={ dish.name } image= { dish.image }/>
+  // }
+  
+function Food({ name, image, rating }) {
   return (
-  <h1>I love { props.fav }</h1>
+    <div>
+      <h2>I love { name }</h2>
+      <h4>{ rating }/5.0</h4>
+      <img src={ image } alt= { name } />
+    </div>
   );
+} 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food fav="kimchi"/>
-      <Food fav="라면"/>
-      <Food fav="삼겹살"/>
-      <Food fav="쭈꾸미"/>
+      {foodILike.map(dish => 
+        <Food key={ dish.key } name={ dish.name } image={ dish.image } rating={ dish.rating }/>
+      )}
+      {/* {foodILike.map(renderFood)} */}
     </div>
   );
 }
