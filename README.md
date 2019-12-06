ccda4a5e1d082c4cd4300f501ef26fc92f71d904
jungsehwan <jungsehwan0612@gmail.com>
2019-12-06 21:48:00 +0900

    191206| State 기초 공부

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ YouTube와 nomadcoders 에서 강의를 확인할 수 있습니다.
 1. [Requirements](1.Requirements.md)
 2. [How React Works](2.How_React_Works.md)
 3. [Components](3.Component.md)
+4. [State](4.State.md)
 
--- src/App.js
+++ src/App.js
@@ -1,60 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
+class App extends React.Component {
 
-const foodILike = [
-  {
-    key: 1,
-    name: "김치",
-    image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5.0
-  },
-  
-  {
-    key: 2,
-    name: "삼겹살",
-    image: "https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg",
-    rating: 4.0
-  },
-  
-  {
-    key: 3,
-    name: "라면",
-    image: "http://file.mk.co.kr/meet/neds/2017/09/image_readtop_2017_587233_15042337473013492.jpg",
-    rating: "3.7"
+  state = {
+    count: 0
   }
-]
 
-// function renderFood(dish) {
-  //   // console.log(dish)
-  //   return <Food name={ dish.name } image= { dish.image }/>
-  // }
-  
-function Food({ name, image, rating }) {
-  return (
+  plus = () => {
+    console.log("plus")
+    // this.state.count += 1
+    this.setState({ count: this.state.count+1 })
+    // this.setState(current => ({ count: current.count +1 }))
+  };
+  minus = () => {
+    console.log("minus")
+    // this.state.count -= 1
+    this.setState({ count: this.state.count-1 })
+    // this.setState(current => ({ count: current.count -1 }))
+  };
+  render() {
+    return (
     <div>
-      <h2>I love { name }</h2>
-      <h4>{ rating }/5.0</h4>
-      <img src={ image } alt= { name } />
+      <h1>I am a class component</h1>
+      <h4>The number is { this.state.count }</h4>
+      <button onClick={ this.plus }>plus</button>
+      <button onClick = {this.minus }>minus</button>
     </div>
-  );
-} 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => 
-        <Food key={ dish.key } name={ dish.name } image={ dish.image } rating={ dish.rating }/>
-      )}
-      {/* {foodILike.map(renderFood)} */}
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
