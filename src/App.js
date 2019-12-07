f2a433edcc1de10c39031b8a186e8dd5831b9999
jungsehwan <jungsehwan0612@gmail.com>
2019-12-07 15:16:34 +0900

    191207| Movie 데이터에서 제목 가져오는 것까지 공부

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
 
@@ -7,20 +9,41 @@ class App extends React.Component {
     movies: []
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 6000)
-  }
-  render() {
+  getMovies = async () => {
     
-    return (
-    <div>
-      { this.state.isLoading ? "Loading..." : "We are ready" }
-    </div>
-    )
+    const { data:
+      { data:
+        { movies }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating')
+    this.setState({ movies, isLoading: false })
+    
   }
 
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          )
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
